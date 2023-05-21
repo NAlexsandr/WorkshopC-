7ceb5f92623703294c324a5da89ad93db3485801
Nikeshin <Nikeshin Alexandr>
2023-05-21 15:08:04 +0700

    Added modified Task03/

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,12 +1,23 @@
-﻿// Написать программу, которая проверяет, является ли первое число квадратомм второго, числа вводит пользователь
+﻿// Написать программу, которая будет выдавать название Дня Недели по заданному ноимеру
 
-Console.WriteLine("!!!Программа проверяет, является ли первое число квадратом второго!!!");
-Console.WriteLine("Введите Первое целое число:");
-int numberА = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите Второе целое число:");
-int numberB = Convert.ToInt32(Console.ReadLine());
-if (numberА == numberB * numberB)
-{
-    Console.WriteLine("Да");
-}
-else Console.WriteLine("Нет");
+Console.WriteLine("!!!Программа выдает название Дня Недели по заданному ноимеру от 1 до 7)!!!");
+Console.WriteLine("Введите Целое число:");
+int DayofWeek = Convert.ToInt32(Console.ReadLine());
+if (DayofWeek > 7)
+    Console.WriteLine("Ошибка. Неверный ввод. Введите целое число в диапазоне от 1 до 7");
+if (DayofWeek < 1)
+    Console.WriteLine("Ошибка. Неверный ввод. Введите целое число в диапазоне от 1 до 7");
+if (DayofWeek == 1)
+    Console.WriteLine("День недели: Понедельник");
+if (DayofWeek == 2)
+    Console.WriteLine("День недели: Вторник");
+if (DayofWeek == 3)
+    Console.WriteLine("День недели: Среда");
+if (DayofWeek == 4)
+    Console.WriteLine("День недели: Четверг");
+if (DayofWeek == 5)
+    Console.WriteLine("День недели: Пятница");
+if (DayofWeek == 6)
+    Console.WriteLine("День недели: Суббота");
+if (DayofWeek == 7)
+    Console.WriteLine("День недели: Воскресенье");
