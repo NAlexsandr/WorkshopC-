708182ca0303bb5c857f0dbde05acbf1d8a6f77f
Nikeshin <Nikeshin Alexandr>
2023-05-21 14:48:36 +0700

    Added modified Task01/

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,5 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите целое число ");
-int number = Convert.ToInt32(Console.ReadLine());
-int square = number * number;
-Console.WriteLine($"Квадрат числа {number} равен {square}");
+﻿// Написать программу, которая проверяет, является ли первое число квадратомм второго, числа вводит пользователь
+
+Console.WriteLine("!!!Программа проверяет, является ли первое число квадратом второго!!!");
+Console.WriteLine("Введите Первое целое число:");
+int numberА = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Второе целое число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberА == numberB * numberB)
+{
+    Console.WriteLine("Да");
+}
+else Console.WriteLine("Нет");
