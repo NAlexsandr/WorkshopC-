f9f6e7d319b82596e899c390c05d35f91c2103da
Nikeshin <Nikeshin Alexandr>
2023-05-21 14:34:46 +0700

    Added Task01/

---
+++ Task01/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {square}");
