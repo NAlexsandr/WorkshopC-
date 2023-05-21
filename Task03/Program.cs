6193521e97759fbc95db2670ac3440f1cb76a2e6
Nikeshin <Nikeshin Alexandr>
2023-05-21 14:51:47 +0700

    Added Task03/

---
+++ Task03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу, которая проверяет, является ли первое число квадратомм второго, числа вводит пользователь
+
+Console.WriteLine("!!!Программа проверяет, является ли первое число квадратом второго!!!");
+Console.WriteLine("Введите Первое целое число:");
+int numberА = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Второе целое число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberА == numberB * numberB)
+{
+    Console.WriteLine("Да");
+}
+else Console.WriteLine("Нет");
