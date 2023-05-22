7a9d1a1be196d3410c640a9a9cadd4b0b90462f3
Nikeshin <Nikeshin Alexandr>
2023-05-22 15:40:39 +0700

    Added modified Task002/

---
+++ Task002/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Программа сравнит два введенных Вами числа и напишет большее число");
+Console.WriteLine("Начали ...");
+Console.WriteLine("Введите Первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+    Console.WriteLine($"Большее число из введенных {number1} ");
+else
+{
+    Console.WriteLine($"Большее число из введенных {number2} ");
+}
