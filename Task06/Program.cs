c357a0c0e7a93ea4e8e791f239ec0c52f442c113
Nikeshin <Nikeshin Alexandr>
2023-05-22 16:12:45 +0700

    Added modified Task06/

---
+++ Task06/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Программа, которая на вход принимает число и выдаёт, является ли число чётным!");
+Console.WriteLine("Начали ...");
+Console.WriteLine("Введите Число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int remainder = number % 2;
+if (remainder == 0)
+    Console.WriteLine("Да, число четное");
+else
+{
+    Console.WriteLine("Нет, число не является четным ");
+}
