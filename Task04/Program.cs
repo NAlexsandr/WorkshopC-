f16ea59981913d4ed6ef0ad8e2642346f5c51af4
Nikeshin <Nikeshin Alexandr>
2023-05-22 15:52:24 +0700

    Added modified Task04/

---
+++ Task04/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Программа принимает на вход 3 числа и выдает Максимальное из них");
+Console.WriteLine("Начали ...");
+Console.WriteLine("Введите Первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > max)
+    max = number2;
+if (number3 > max)
+    max = number3;
+Console.WriteLine($"Максимальное из 3-х чисел: {max} ");
