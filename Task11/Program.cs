cd08e8294679acb8be1603aee786d1b18a614dde
Nikeshin <Nikeshin Alexandr>
2023-05-23 16:23:55 +0700

    Added modified Task11/

---
+++ Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка [100, 999] -> {number}");
+
+int res = DeleteSecondDigit(number);
+Console.WriteLine($"Итог -> {res}");
+
+int DeleteSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+
+    int result = firstDigit * 10 + lastDigit;
+    return result;
+}
