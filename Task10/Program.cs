2bf572933ca0a11f6e25c463b619a3381244a0e7
Nikeshin <Nikeshin Alexandr>
2023-05-25 15:22:27 +0700

    Added modification Task10/

---
+++ Task10/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int SecondNumber(int num)
+    {
+        int second = num / 10;
+        second = second % 10;
+        return second;
+        
+    }
+    
+Console.WriteLine("Ведите трехзначное число и получите на выходе ВТОРУЮ цифру чиcла: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+ Console.WriteLine($"Вторая цифра числа {SecondNumber(number)}");
+ else
+ {
+    Console.WriteLine("Число введено некорректно");
+ };
+
+
+    
