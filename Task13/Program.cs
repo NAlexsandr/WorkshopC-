772b9ef2b4e0092ca35c6f4a6f39a7b444999c5f
Nikeshin <Nikeshin Alexandr>
2023-05-25 16:22:10 +0700

    Added modification Task13/

---
+++ Task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+int ThreeNumber(int num)
+{
+    while (num >= 1000)
+    {
+        num = num / 10;
+    }
+
+    int three = num % 10;
+    return three;
+}
+
+Console.WriteLine("Ведите любое ЦЕЛОЕ чиcло: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+    Console.WriteLine($"ТРЕТЬЯ цифра числа {ThreeNumber(number)}");
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+};
